7729fe0aa6b039525becd4ed1e60d6154a5f3f22
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-06-14 03:24:34 +0330

    feat: use fullstacksjs' eslint, prettier, and tsconfig configs

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+const { init } = require('@fullstacksjs/eslint-config/init');
+
+module.exports = init({
+  root: true,
+  modules: {
+    auto: true,
+    react: true,
+    next: true,
+    cspell: true,
+    typescript: {
+      parserProject: './tsconfig.json',
+      resolverProject: './tsconfig.json',
+    },
+  },
+  rules: {
+    'tailwindcss/no-custom-classname': ['warn', { whitelist: ['size\\-.*'] }],
+    'default-case': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+  },
+});
