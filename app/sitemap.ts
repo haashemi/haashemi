39cbe97d61cb577a663ef4df032bf047ae2c0066
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-08-01 18:01:10 +0330

    chore: format a few files

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.haashemi.dev',
+      url: "https://www.haashemi.dev",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 1,
     },
   ];
