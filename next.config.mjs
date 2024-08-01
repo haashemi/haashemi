76d5a1746aba429e95adaaaf900641d387ddfe67
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-08-01 23:10:41 +0330

    chore: disable next.js' image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin("./app/i18n.ts");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: { unoptimized: true },
 };
 
 export default withNextIntl(nextConfig);
