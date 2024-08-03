8ad2d261b3adeb5e833cc1af507cc623f4b608c6
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-08-03 21:42:04 +0330

    feat: make contact cards accessible

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -86,13 +86,15 @@ const BlogPostCard = (props: BlogPostCardProps) => (
 
 interface ContactCardProps {
   Icon: IconType;
+  ariaLabel:string;
   user: string;
   url: string;
 }
 
-const ContactCard = ({ Icon, user, url }: ContactCardProps) => (
+const ContactCard = ({ Icon, ariaLabel, user, url }: ContactCardProps) => (
   <a
     href={url}
+    aria-label={ariaLabel}
     className="group flex aspect-video w-full flex-col items-center justify-center gap-5 rounded-xl border-2 border-zinc-900 bg-zinc-950 transition-all hover:border-zinc-950 hover:bg-zinc-900 focus:border-zinc-700 focus:bg-black sm:aspect-square"
   >
     <Icon className="size-1/3 text-zinc-200 transition-colors group-hover:text-white" />
