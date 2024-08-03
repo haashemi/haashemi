faefa2c452cc69d0d5b223ca5b0f51eca4138bc2
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-08-03 20:43:41 +0330

    feat: add Contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import { useTranslations } from "next-intl";
+import { FaGithub, FaInstagram, FaLinkedin, FaTelegram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { GoMail } from "react-icons/go";
+import { MdOutlineEmail } from "react-icons/md";
 
 import Section from "@/app/_components/Section";
 
-import { BlogPostCard, ProjectCard } from "./_components/Card";
+import { ContactCard, ProjectCard } from "./_components/Card";
 
 const ProfilePage = () => {
   const t = useTranslations("Home");
 
   return (
-    <div className="flex flex-col gap-12 p-3 py-16">
+    <main className="container mx-auto flex flex-col gap-12 p-3 py-16">
       <div className="flex min-h-[50vh] flex-col justify-center gap-10">
         <div className="flex flex-col">
           <h2 className="ml-6 text-2xl font-bold opacity-70">Golang</h2>
@@ -45,7 +49,8 @@ const ProfilePage = () => {
         />
       </Section>
 
-      <Section isGrid title={t("blogPostsTitle")} desc={t("blogPostsDesc")}>
+      {/* TODO: */}
+      {/* <Section isGrid title={t("blogPostsTitle")} desc={t("blogPostsDesc")}>
         <BlogPostCard id={3} title="The bug I found in Golang's stdlib for the first-time!" publishedAt="18/05/2024" />
         <BlogPostCard
           id={2}
@@ -53,12 +58,24 @@ const ProfilePage = () => {
           publishedAt="23/03/2024"
         />
         <BlogPostCard id={1} title="Painter now supports all type of gradients!" publishedAt="01/11/2023" />
-      </Section>
+      </Section> */}
 
-      <Section title={t("experiencesTitle")} desc={t("experiencesDesc")}>
+      {/* TODO: */}
+      {/* <Section title={t("experiencesTitle")} desc={t("experiencesDesc")}>
         <div>TODO:</div>
+      </Section> */}
+
+      <Section title="Contact" desc="Let's keep in touch!">
+        <div className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          <ContactCard Icon={MdOutlineEmail} user="contact@haashemi.dev" url="mailto:contact@haashemi.dev" />
+          <ContactCard Icon={FaLinkedin} user="haashemi" url="https://www.linkedin.com/in/haashemi" />
+          <ContactCard Icon={FaTelegram} user="Byfron" url="https://t.me/Byfron" />
+          <ContactCard Icon={FaGithub} user="haashemi" url="https://github.com/haashemi" />
+          <ContactCard Icon={FaXTwitter} user="haashemi_dev" url="https://x.com/haashemi_dev" />
+          <ContactCard Icon={FaInstagram} user="haashemi_dev" url="https://instagram.com/haashemi_dev" />
+        </div>
       </Section>
-    </div>
+    </main>
   );
 };
 
