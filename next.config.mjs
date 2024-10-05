b1de224c79b506653452815fc2dc4582fad80023
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-10-06 01:26:32 +0330

    feat: set the output to export

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@ import { withContentCollections } from "@content-collections/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    formats: ["image/avif", "image/webp"],
-    minimumCacheTTL: 3600, // an hour
-  },
+  output: "export",
 };
 
 export default withContentCollections(nextConfig);
