40691a21b7b2e2c91606151c210f88902974a89c
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-10-06 01:53:10 +0330

    feat: add work experiences section

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,26 @@
-import { allProjects } from "content-collections";
+import { allWorks } from "content-collections";
 
 import { Section } from "./_components/section";
-import { TechStackSection } from "./_components/tech-stack";
+import { TechStack, techStack } from "./_components/tech-stack";
+import { WorkExperience } from "./_components/work-experience";
 
 export default function Home() {
   return (
     <main className="flex w-full flex-col gap-5">
-      <TechStackSection />
+      <Section title="Tech Stack">
+        <div className="flex flex-wrap gap-3 px-3">
+          {techStack.map((ts) => (
+            <TechStack key={ts.title} title={ts.title} Icon={ts.Icon} />
+          ))}
+        </div>
+      </Section>
 
-      <Section title="Projects">
+      <Section title="Work Experiences">
         <div className="flex flex-col gap-3 px-2">
-          {allProjects
-            .toSorted((a, b) => a.priority - b.priority)
-            .map((v) => (
-              // TODO: Link to its own page.
-              <a
-                key={v._meta.path}
-                aria-label={v.name}
-                href={v.githubUrl}
-                className="group relative flex-1 border-l-2 border-zinc-800 px-4 py-2 transition-colors duration-300 hover:border-zinc-500"
-              >
-                <span className="absolute left-0 top-0 size-full bg-gradient-to-r from-zinc-900 to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
-
-                <div className="relative flex flex-col gap-2">
-                  <span>{v.name}</span>
-                  <p className="text-sm text-zinc-500">{v.summary}</p>
-                </div>
-              </a>
+          {allWorks
+            .toSorted((a, b) => b.startDate.getTime() - a.startDate.getTime())
+            .map((work) => (
+              <WorkExperience key={work._meta.path} work={work} />
             ))}
         </div>
       </Section>
