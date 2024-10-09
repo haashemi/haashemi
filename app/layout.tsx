8d4d13fecb6bee9fa8e146557adad6f5aa2b480d
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-10-09 22:35:44 +0330

    style: change the font from Inter to Space Grotesk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import "./globals.css";
 
 import type { Metadata, Viewport } from "next";
-import { Inter } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 
 import { Header } from "./_components/header";
 import { cn } from "./_lib/cn";
 
-const font = Inter({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
+const font = Space_Grotesk({
   display: "swap",
+  subsets: ["latin"],
+  adjustFontFallback: true,
 });
 
 export const metadata: Metadata = {
@@ -30,6 +30,10 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <div className="relative mx-auto w-full max-w-prose">
           <Header />
           {children}
+          <div className="h-20" />
+
+          <div className="fixed left-0 top-0 h-10 w-full bg-gradient-to-b from-zinc-950 to-transparent" />
+          <div className="fixed bottom-0 left-0 h-10 w-full bg-gradient-to-t from-zinc-950 to-transparent" />
         </div>
       </body>
     </html>
