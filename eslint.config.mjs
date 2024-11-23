47ab5e743993b326242b0e951fd148764d71dc30
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-11-24 01:09:01 +0330

    fix: temporarily fix eslint's fatal error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,7 @@ import { init } from "@fullstacksjs/eslint-config";
 
 export default init({
   ignores: [".content-collections"],
+  rules: {
+    "@typescript-eslint/no-empty-function": "error",
+  },
 });
