5352dd693ec3bc53b7c4d9e37a08c3aa62649984
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-11-24 02:43:34 +0330

    fix: fix wrong config import

--- app/urls/telegram-channel/route.ts
+++ app/urls/telegram-channel/route.ts
@@ -1,5 +1,5 @@
-import { URL_TELEGRAM_CHANNEL } from "@/app/_lib/config.server";
+import { config } from "@/app/_lib/config.server";
 
 export function GET(_: Request) {
-  return Response.redirect(URL_TELEGRAM_CHANNEL, 301);
+  return Response.redirect(config.get("urls.telegramChannel"), 301);
 }
