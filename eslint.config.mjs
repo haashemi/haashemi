6d0bab8bc3a46a7d1d9c162b374a08ca9bfd3488
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-12-21 00:33:56 +0330

    fix: remove the eslint rule as it's fixed by upstream

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,3 @@
 import { init } from "@fullstacksjs/eslint-config";
 
-export default init({
-  ignores: [".content-collections"],
-  rules: {
-    "@typescript-eslint/no-empty-function": "error",
-  },
-});
+export default init({ ignores: [".content-collections"] });
