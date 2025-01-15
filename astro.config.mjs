ca48f85fe0be5f5a16b8310e07fc546250ace9c3
Ali Hashemi <mr.ali.haashemi@gmail.com>
2025-01-15 22:06:42 +0330

    feat: add github-blockquote-alert

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import { remarkAlert } from "remark-github-blockquote-alert";
 
 export default defineConfig({
   site: "https://haashemi.dev",
   integrations: [mdx(), sitemap(), tailwind(), icon()],
+  markdown: {
+    remarkPlugins: [remarkAlert],
+  },
 });
