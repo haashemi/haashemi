df138c1756d5799b06b34a28d8919b46769de40d
Ali Hashemi <mr.ali.haashemi@gmail.com>
2025-01-17 20:48:59 +0330

    fix: improve editor configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,12 @@ import { init } from "@fullstacksjs/eslint-config";
 import eslintPluginAstro from "eslint-plugin-astro";
 
 export default [
-  ...eslintPluginAstro.configs["flat/recommended"],
   ...init(),
+  ...eslintPluginAstro.configs["flat/recommended"],
   {
-    ignores: [".astro/*", "dist/*"],
-  },
-  {
-    files: ["**/*.astro"],
+    files: ["**/*.astro", "**/*.ts"],
     rules: {
-      "astro/no-set-text-directive": "error",
-      "astro/no-unused-css-selector": "error",
-      "import/no-unresolved": ["error", { ignore: ["astro:content"] }],
+      "import/no-unresolved": ["error", { ignore: ["astro:content", "astro:transitions"] }],
     },
   },
 ];
