ec3c9b42906b65416b7b5d9b3f515e069293b077
Ali Hashemi <mr.ali.haashemi@gmail.com>
2025-03-16 16:34:23 +0330

    feat: improve linting stuff

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,23 @@
+import { init } from "@fullstacksjs/eslint-config";
 import eslintPluginAstro from "eslint-plugin-astro";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default [...eslintPluginAstro.configs.recommended, eslintPluginPrettierRecommended];
+export default defineConfig([
+  ...eslintPluginAstro.configs.base,
+  ...init({
+    react: false, // It thinks that .astro files are react files. so let's just disable it.
+    tailwind: false, // At the moment it doesn't support tailwind v4.
+  }),
+  globalIgnores([".astro"]),
+  {
+    rules: {
+      // We'll wait for @fullstacksjs' astro support
+      "import/no-unresolved": [
+        "error",
+        {
+          ignore: ["^astro:.*"],
+        },
+      ],
+    },
+  },
+]);
